b66e1e569f43b0c0700ad964d881f84bba25b4a1
Rayner Villalba <rayner.villalba@mojix.com>
2025-12-29 17:02:41 -0400

    Adding image validation

--- src/ingest.py
+++ src/ingest.py
@@ -7,15 +7,28 @@ from datetime import datetime
 sqs = boto3.client("sqs")
 
 QUEUE_URL = os.environ["QUEUE_URL"]
+ALLOWED_EXTENSIONS = (".png", ".jpg", ".jpeg")
 
 def lambda_handler(event, context):
-  for record in event["Records"]:
-    bucket = record["s3"]["bucket"]["name"]
-
+  records = event.get("Records", [])
+  
+  if not records:
+    print("No records found in event")
+    return {
+        "statusCode": 200, 
+        "body": "No records to process"
+    }
+  
+  for record in records:
     key = urllib.parse.unquote_plus(
       record["s3"]["object"]["key"]
     )
 
+    if not key.lower().endswith(ALLOWED_EXTENSIONS):
+      print("Skipping NOT valid Image:", key)
+      continue
+
+    bucket = record["s3"]["bucket"]["name"]
     etag = record["s3"]["object"].get("eTag")
 
     message = {
